176410bed08906d8e5e8a7ec62b6032ec724a44c
unknown <chickenman4236@gmail.com>
2019-12-02 15:00:28 +0100

    KeyboardAxis working

---
+++ ShadowBuild/Input/Axis/Axis.cs
@@ -0,0 +1,46 @@
+﻿using ShadowBuild.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowBuild.Input.Axis
+{
+    public abstract class Axis
+    {
+        private static List<Axis> axes = new List<Axis>();
+
+        public string name;
+        public double value;
+
+        public static void setup(Axis axis)
+        {
+            axes.Add(axis);
+        }
+        private static Axis getAxisByName(string name)
+        {
+            foreach (Axis axis in axes)
+                if (axis.name == name) return axis;
+            return null;
+        }
+        public static double getValue(string name)
+        {
+            Axis axis = getAxisByName(name);
+            if (axis == null) throw new AxisNotFoundException();
+
+            double value = 0;
+
+            if(axis is KeyboardAxis)
+            {
+                KeyboardAxis ka = (KeyboardAxis)axis;
+                if (Keyboard.keyPressed(ka.minusValue))
+                    value--;
+                if (Keyboard.keyPressed(ka.plusValue))
+                    value++;
+
+            }
+            return value;
+        }
+    }
+}
---
+++ ShadowBuild/Input/Axis/KeyboardAxis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShadowBuild.Input.Axis
+{
+    public class KeyboardAxis : Axis
+    {
+        internal Keys minusValue;
+        internal Keys plusValue;
+
+        public KeyboardAxis(string name, Keys minusValue, Keys plusValue)
+        {
+            this.minusValue = minusValue;
+            this.plusValue = plusValue;
+            this.name = name;
+        }
+    }
+}
