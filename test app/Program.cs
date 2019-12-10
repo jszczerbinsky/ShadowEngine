f0f58c916fddac5da909fa33a96b6d22bbede40b
unknown <chickenman4236@gmail.com>
2019-12-10 08:52:51 +0100

    Fixed test app

---
+++ test app/Program.cs
@@ -0,0 +1,17 @@
+﻿using ShadowBuild;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+    }
+}
